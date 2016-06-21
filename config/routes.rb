7c0d826db74f524bf2acc35098bfb4144c2a8660
Olgaliber <olgaliberchuk@gmail.com>
2016-06-21 06:28:48 +0000

    generating users controller and users/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 RottenMangoes::Application.routes.draw do
   
+  get "users/new"
+  get "users/create"
   Rails.application.routes.draw do
+  get "users/new"
+  get "users/create"
     resources :movies
+    resources :users, only: [:new, :create]
   end
     
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621055502) do
+ActiveRecord::Schema.define(version: 20160621060449) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20160621055502) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
 end
